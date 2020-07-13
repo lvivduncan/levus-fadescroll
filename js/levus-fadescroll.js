6bc342f973e1e73af66303c8d719d306cbeec8ad
lvivduncan <lvivduncan@gmail.com>
2020-07-13 22:45:43 +0300

    fadescroll start

---
+++ js/levus-fadescroll.js
@@ -0,0 +1,22 @@
+// 13-07-2020
+{
+    const fadescroll = document.querySelectorAll('#levus-fadescroll div');
+
+    const left = document.querySelector('#left');
+    const right = document.querySelector('#right');
+
+    let num = 0;
+    const length = fadescroll.length - 1;
+
+    left.addEventListener('click', () => {
+        fadescroll.forEach(item => item.style.opacity = 0);
+        fadescroll[num].style.opacity = 1;
+        num <= 0 ? num = length : num--;
+    });
+
+    right.addEventListener('click', () => {
+        fadescroll.forEach(item => item.style.opacity = 0);
+        fadescroll[num].style.opacity = 1;
+        num < length ? num++ : num = 0;
+    });
+}
