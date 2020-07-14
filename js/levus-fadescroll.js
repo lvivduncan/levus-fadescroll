f6911eac0f767e9a4fc2199fc3016ad0677407ba
lvivduncan <lvivduncan@gmail.com>
2020-07-14 11:25:02 +0300

    add resize

--- js/levus-fadescroll.js
+++ js/levus-fadescroll.js
@@ -1,24 +1,27 @@
-// 13-07-2020
+// 14-07-2020
 {
-    const fadescroll = document.querySelectorAll('#levus-fadescroll div');
-
+    const fadescroll = document.querySelector('#levus-fadescroll');
+    const slides = document.querySelectorAll('#levus-fadescroll div');
     const left = document.querySelector('#left');
     const right = document.querySelector('#right');
 
     let num = 0;
-    const length = fadescroll.length - 1;
+    const length = slides.length - 1;
 
     left.addEventListener('click', () => {
-        fadescroll.forEach(item => item.style.opacity = 0);
-        fadescroll[num].style.opacity = 1;
+        slides.forEach(item => item.style.opacity = 0);
+        slides[num].style.opacity = 1;
         num <= 0 ? num = length : num--;
     });
 
     right.addEventListener('click', () => {
-        fadescroll.forEach(item => item.style.opacity = 0);
-        fadescroll[num].style.opacity = 1;
+        slides.forEach(item => item.style.opacity = 0);
+        slides[num].style.opacity = 1;
         num < length ? num++ : num = 0;
     });
-}
 
-// todo: resize height and width
+    // resize scroll
+    window.addEventListener('resize', () => {
+        fadescroll.style.height = `${fadescroll.clientWidth * 3 / 4}px`;
+    });
+}
